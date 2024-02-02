4aa8f9441b8e19222b73b58bb30541ff1013cf85
alxwang <alxwang@gmail.com>
2024-02-02 13:51:45 -0600

    Review

--- pop/main.c
+++ pop/main.c
@@ -63,21 +63,78 @@ int myIndexOfwithCountandPos(char * str, char ch,int  * pcount,char **lastPos)
     return iPos;
 }
 
+void review(){
+    int iArr[]={1,2,3,4,5,6};
+    char * cPtr=(char*)iArr;
+    int * iPtr=iArr;
+
+    printf("%p = %p = %p = %p\n",&iArr[0],iArr,cPtr,iPtr);
+
+    for(int i=0;i<6;i++) printf("%d ",iArr[i]);
+    printf("\n");
+
+    for(int * pCur = iPtr;pCur-iPtr<6;pCur++)printf("%d ",*pCur);
+    printf("\n");
+
+    int * pCur = iPtr;
+    while(pCur-iPtr<6)
+    {
+        printf("%d ",*pCur);
+        pCur++;
+    }
+    printf("\n");
+
+    pCur = iPtr;
+//    for(int i=0;i<6;i++)
+    for(;;)//a never ending loop
+    {
+        (*pCur)--;
+        pCur++;
+        if(pCur-iPtr>5) break;
+    }
+    for(int i=0;i<6;i++) printf("%d ",iArr[i]);
+    printf("\n");
+    printf("%p - %p = %d\n",pCur,iPtr,pCur-iPtr);
+    pCur-=7;//Move pCur back to the 1st element of array
+    printf("%p - %p = %d\n",pCur,iPtr,pCur-iPtr);
+
+    printf("%p = %p = %p = %p\n",&iArr[0],iArr,cPtr,iPtr);
+    *cPtr = 7;//Update the first lowest byte of first element of array ato 7
+
+    cPtr++;
+    *cPtr = 7;//0x00000707
+
+    cPtr++;
+    *cPtr = 7;//0x00070707
+
+    cPtr++;
+    *cPtr = 7;//0x07070707
+
+    cPtr++;
+    //Update the first lowest byte of second element of array ato 7
+    *cPtr = 7;//0x07070707
+
+    for(int i=0;i<6;i++) printf("%d ",iArr[i]);
+
+
+}
+
+
 
 int main() {
-    char cTestString[] = "It's a beautiful day, isn't it?";
-    int iLoc=0;
+//    char cTestString[] = "It's a beautiful day, isn't it?";
+//    int iLoc=0;
+//
+//    iLoc= myIndexOf(cTestString,'a');
+//    printf("%c is in %s at %d\n",'a',cTestString,iLoc);
+//
+//    int count = 0;
+//    iLoc= myIndexOfwithCount(cTestString,'a',&count);
+//    printf("(%d) %c is in %s at %d\n",count,'a',cTestString,iLoc);
+//
+//    char * lastPos = NULL;
+//    iLoc= myIndexOfwithCountandPos(cTestString,'a',&count,&lastPos);
+//    printf("(%d) %c is in \"%s\" at (%d)\"%s\"\n",count,'a',cTestString,iLoc,lastPos);
 
-    iLoc= myIndexOf(cTestString,'a');
-    printf("%c is in %s at %d\n",'a',cTestString,iLoc);
-
-    int count = 0;
-    iLoc= myIndexOfwithCount(cTestString,'a',&count);
-    printf("(%d) %c is in %s at %d\n",count,'a',cTestString,iLoc);
-
-    char * lastPos = NULL;
-    iLoc= myIndexOfwithCountandPos(cTestString,'a',&count,&lastPos);
-    printf("(%d) %c is in \"%s\" at (%d)\"%s\"\n",count,'a',cTestString,iLoc,lastPos);
-
-
+    review();
 }
