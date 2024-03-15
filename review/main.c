48aab5e4d3455dec67ac34c498c2b22d570d2fef
alxwang <alxwang@gmail.com>
2024-03-15 13:42:37 -0600

    Review

--- review/main.c
+++ review/main.c
@@ -1,6 +1,61 @@
+
 #include <stdio.h>
 
+#define	ENGLISH_SIGNAL	0x01
+#define	FRENCH_SIGNAL	0x02
+#define	CREE_SIGNAL		0x04
+
+
+unsigned char languageQuestions()
+{
+    unsigned char lang = 0;//All 8 bits are 0 so all false
+    unsigned char choice[] = "YES";
+    printf("Do you speak english(y/n)?");
+    fgets(choice,3,stdin);
+    if (choice[0]=='y')
+    {
+        //set first bit in lang to 1
+        lang |= ENGLISH_SIGNAL;
+    }
+    printf("Do you speak French(y/n)?");
+    fgets(choice,3,stdin);
+    if (choice[0]=='y')
+    {
+        //set first bit in lang to 1
+        lang |= FRENCH_SIGNAL;
+    }
+
+    printf("Do you speak Plains Cree(y/n)?");
+    fgets(choice,3,stdin);
+    if (choice[0]=='y')
+    {
+        //set first bit in lang to 1
+        lang |= CREE_SIGNAL;
+    }
+    return  lang;
+}
+
+
+void printGreetings(unsigned  char lang)
+{
+    if(lang & ENGLISH_SIGNAL)
+    {
+        printf("Hello\n");
+    }
+    if(lang & FRENCH_SIGNAL)
+    {
+        printf("Bonjour\n");
+    }
+    if(lang & CREE_SIGNAL)
+    {
+        printf("Tan'si\n");
+    }
+}
+
+
+
 int main() {
-    printf("Hello, World!\n");
+    unsigned  char lang = languageQuestions();
+    printGreetings(lang);
     return 0;
 }
