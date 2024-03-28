d403c90ed2dc0448483ae5d0ede9865f3f6c0f45
alxwang <alxwang@gmail.com>
2024-03-28 14:05:41 -0600

    Student Classroom File IO start files

--- student/fileio.c
+++ student/fileio.c
@@ -7,15 +7,42 @@
 
 FILE* openFile(const char* fileName, const char* fileMode)
 {
-
+    FILE * f = NULL;
+    if((f= fopen(fileName,fileMode))!=NULL)
+    {
+        printf("File opened\n");
+    }
+    else
+    {
+        printf("File open failed\n");
+    }
+    return f;
 }
 
 int readFile(FILE* filePtr, unsigned char * data, int bytesToRead)
 {
-
+    int nRead = fread(data,sizeof(char),bytesToRead,filePtr);
+    if(nRead!=bytesToRead)
+    {
+        printf("Try to read %d bytes, got %d bytes\n",bytesToRead,nRead);
+        return ferror(filePtr);
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int writeFile(FILE* filePtr, unsigned char * data, int bytesToWrite)
 {
-
+    int numWrite = fwrite(data,sizeof(char),bytesToWrite,filePtr);
+    if(numWrite!=bytesToWrite)
+    {
+        printf("Try to write %d bytes, write %d bytes\n",bytesToWrite,numWrite);
+        return ferror(filePtr);
+    }
+    else
+    {
+        return 0;
+    }
 }
--- student/main.c
+++ student/main.c
@@ -3,27 +3,12 @@
 #include <string.h>
 #include "student.h"
 
-typedef struct {
-    char a;
-    char b;
-    int i;
-} S1;
-
-typedef struct {
-    char a[5];
-    short d;
-    short e;
-    int i;
-    char b;
-} S2;
 
 
 
 int main() {
-    printf("%d\n",sizeof(S1));
-    printf("%d\n",sizeof(S2));
-//    classroom  * c = PopulateClassroom();
-//    DisplayClassroom(c);
-//    FreeClassroom(c);
+    classroom  * c = PopulateClassroom();
+    DisplayClassroom(c);
+    FreeClassroom(c);
     return 0;
 }
