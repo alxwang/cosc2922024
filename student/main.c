984bf556aff0d7719fb6cf8e17fb97dc9e0bcde1
alxwang <alxwang@gmail.com>
2024-04-05 14:49:55 -0600

    Student Classroom File IO All done

--- student/main.c
+++ student/main.c
@@ -76,11 +76,54 @@ void displayValuePtr(computer *  c)
     printf("%s: %d %c %s\n",c->ID,c->ram,c->ramUnit,c->desc);
 }
 
+//7.	How would you define a union that would contain options for memory to be stored
+//        in bytes (as a long long int, in MB (as an int),
+//                  in GB (as a short), or in TB (as an unsigned char)?
+
+typedef union{
+    long long int bytes; //8 bytes
+    int mb;
+    short gb;
+    unsigned char tb;
+}MemorySize;
+#define BYTE unsigned char
+BYTE mixByte(BYTE byte)
+{
+//    return byte>>6 | byte<<2;//Simple one
+    BYTE rs = byte>>6 | byte<<2;
+    BYTE bit3= (rs & 0x40) >> 1;
+    BYTE bit2=(rs & 0x20) << 1;
+    rs = (rs & 0x9F) | bit2 | bit2;
+    return rs;
+}
+
+BYTE unmixByte(BYTE byte)
+{
+//    return byte >> 2 | byte <<6; //Simple one
+    BYTE rs = byte >> 2 | byte <<6;
+    BYTE bit5 = (rs & 0x10) >>1;
+    BYTE bit4 = (rs & 0x08) <<1;
+    rs = (rs & 0xE7) | bit4 | bit5;
+    return rs;
+}
 
 int main(int argc, char ** argv) {
-    computer  * com = assignValuesPtr("123456789",16,'A',"Not enough ram");
-    displayValuePtr(com);
-    free(com);
+    //8.	What would the size of the union given above be? 8 bytes
+    printf("%d\n",sizeof(MemorySize));
+
+    MemorySize m;
+
+    m.tb = 16; //0x10
+    printf("%x\n", m.bytes); //00000000000000010
+    m.bytes = 1234567890;//0x00000000499602D2
+    printf("%x\n", m.tb);//tb is the lowest byte of the union
+    printf("%x\n", m.gb);//tb is the lowest 2 bytes of the union
+    printf("%x\n", m.mb);//tb is the lowest 4 byte of the union
+
+
+//    computer  * com = assignValuesPtr("123456789",16,'A',"Not enough ram");
+//    displayValuePtr(com);
+//    free(com);
     return 0;
 
 
