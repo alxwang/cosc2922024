7577d48bcdc3c1a4a5abbe6bb1cba2ab91c7df9b
alxwang <alxwang@gmail.com>
2024-04-17 10:13:19 -0600

    Bitmap

--- bitmap/bitmap.c
+++ bitmap/bitmap.c
@@ -1,6 +1,8 @@
 #include "bitmap.h"
 #include <memory.h>
 #include <math.h>
+#include <stdlib.h>
+
 #define MAX_FILENAME_SIZE	256
 //
 // Created by Alex Wang on 2024-04-10.
@@ -19,23 +21,65 @@ FILE* GetFile(char* cPrompt, char* cMode)
     return aFile;
 }
 
-void ReadImage(IMAGE*, FILE*)
+void ReadImage(IMAGE*imgPtr, FILE*f)
 {
-
+    ReadHeader(imgPtr,f);
+    if(imgPtr->bmHDR!=NULL)
+    {
+        ReadData(imgPtr,f);
+    }
 }
 
-void ReadHeader(IMAGE*, FILE*)
+void ReadHeader(IMAGE*imgPtr, FILE*f)
 {
-
+    imgPtr->bmHDR=(BITMAPHDR*)malloc(sizeof(BITMAPHDR));
+    if(imgPtr->bmHDR!=NULL)
+    {
+        if(fread(imgPtr->bmHDR, sizeof(BITMAPHDR),1,f)!=1)
+        {
+            free(imgPtr->bmHDR);
+            imgPtr->bmHDR=NULL;
+            printf("Failed to read header");
+        }
+    }
 }
 
-void ReadData(IMAGE*, FILE*)
+void ReadData(IMAGE* imgPtr, FILE*f)
 {
-
+    unsigned int padding = imgPtr->bmHDR->dwWidth % 4;
+    unsigned int imageSize =
+            (imgPtr->bmHDR->dwWidth*sizeof(PIXEL)+padding)
+            *imgPtr->bmHDR->dwHeight;
+    imgPtr->bmData=(PIXEL*) malloc(imageSize);
+    if(imgPtr->bmData!=NULL)
+    {
+        if(fread(imgPtr->bmData,imageSize,1,f)!=1)
+        {
+            free(imgPtr->bmData);
+            imgPtr->bmData=NULL;
+            free(imgPtr->bmHDR);
+            imgPtr->bmHDR=NULL;
+        }
+    }
+    else
+    {
+        free(imgPtr->bmHDR);
+        imgPtr->bmHDR=NULL;
+    }
 }
 
-void PrintHeader(BITMAPHDR*)
+void PrintHeader(BITMAPHDR*headerPtr)
 {
+    printf("First two characters: %x\n", headerPtr->wType);
+    printf("File size: %d\n", headerPtr->dwFileSize);
+    printf("Data offset: %d\n", headerPtr->dwDataOffset);
+    printf("Header size: %d\n", headerPtr->dwHeaderSize);
+    printf("Dimensions: %d by %d\n", headerPtr->dwWidth, headerPtr->dwHeight);
+    printf("Planes: %d\n", headerPtr->wPlanes);
+    printf("Color depth: %d\n", headerPtr->wBitCount);
+    printf("Compression: %d\n", headerPtr->dwCompression);
+    printf("Image size: %d\n", headerPtr->dwImageSize);
+    printf("Colors used: %d\n", headerPtr->dwClrUsed);
 
 }
 
