47c664b5aa24ec8ad973a69680de0d37dce06c11
alxwang <alxwang@gmail.com>
2024-04-17 10:16:49 -0600

    Bitmap

--- bitmap/main.c
+++ bitmap/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include "bitmap.h"
 int main() {
     FILE * f = GetFile("Please enter a bitmap file:","rb");
-    printf("%x",f);
+    IMAGE img;
+    memset(&img,0,sizeof(IMAGE));
+    ReadImage(&img,f);
+    if(img.bmHDR!=NULL)
+    {
+        PrintHeader(img.bmHDR);
+    }
     return 0;
 }
