29b9250f2eb487f7d9fd046415e6d2ae0b7bb98c
alxwang <alxwang@gmail.com>
2024-04-18 10:14:48 -0600

    Bitmap

--- bitmap/bitmap.c
+++ bitmap/bitmap.c
@@ -83,35 +83,123 @@ void PrintHeader(BITMAPHDR*headerPtr)
 
 }
 
-void ManipulateImage(IMAGE*, BM_FUNC_PTR)
+void ManipulateImage(IMAGE*imgPtr, BM_FUNC_PTR func)
 {
-
+    for(int row =0;row<imgPtr->bmHDR->dwHeight;row++)
+    {
+        for(int col=0;col<imgPtr->bmHDR->dwWidth;col++)
+        {
+            func(GetPixel(row,col,imgPtr));
+        }
+    }
 }
 
 PIXEL* GetPixel(int row, int col, IMAGE* imgPtr)
 {
-
+    unsigned int padding = imgPtr->bmHDR->dwWidth % 4;
+    unsigned int offset =
+            (imgPtr->bmHDR->dwWidth*sizeof(PIXEL)+padding)*row+
+            col*sizeof(PIXEL);
+    return (PIXEL*)(((BYTE*)imgPtr->bmData)+offset);
 }
 
-void ManipulateTwoImages(IMAGE*, IMAGE*, BM_TWO_PIXELS)
+void ManipulateTwoImages(IMAGE* imgPtr1, IMAGE*imgPtr2, BM_TWO_PIXELS func)
 {
-
+    //Apply func to the area of the smaller image
+    //Get work area size
+    int height  = (imgPtr1->bmHDR->dwHeight<imgPtr2->bmHDR->dwHeight)?
+                    imgPtr1->bmHDR->dwHeight:imgPtr2->bmHDR->dwHeight;
+    int width = (imgPtr1->bmHDR->dwWidth<imgPtr2->bmHDR->dwWidth)?
+                imgPtr1->bmHDR->dwWidth:imgPtr2->bmHDR->dwWidth;
+    for(int row =0;row<height;row++)
+    {
+        for(int col=0;col<width;col++)
+        {
+            func(GetPixel(row,col,imgPtr1),GetPixel(row,col,imgPtr2));
+        }
+    }
 }
 
 
-IMAGE ResizeCanvas(IMAGE*, int, int)
+IMAGE ResizeCanvas(IMAGE*imgPtr, int newWidth, int newHeight)
 {
-
+    IMAGE imgNew={NULL,NULL};
+    unsigned int newImageSize = 0;
+    unsigned int padding = 0;
+    unsigned int width,height;
+    PIXEL startPixel={255,255,255};
+    imgNew.bmHDR=(BITMAPHDR*) malloc(sizeof(BITMAPHDR));
+    if(imgNew.bmHDR)
+    {
+        memcpy(imgNew.bmHDR,imgPtr->bmHDR,sizeof(BITMAPHDR));
+        imgNew.bmHDR->dwWidth=newWidth;
+        imgNew.bmHDR->dwHeight=newHeight;
+        padding = newWidth % 4;
+        newImageSize = (newWidth*sizeof(PIXEL)+padding)*newHeight;
+        imgNew.bmHDR->dwImageSize=newImageSize;
+        imgNew.bmHDR->dwFileSize=newImageSize+ sizeof(BITMAPHDR);
+        imgNew.bmData=(PIXEL*) malloc(newImageSize);
+        if(imgNew.bmData)
+        {
+            InitializePixelData(&imgNew,startPixel);
+            height=(newHeight<imgPtr->bmHDR->dwHeight)?newHeight:imgPtr->bmHDR->dwHeight;
+            width=(newWidth<imgPtr->bmHDR->dwWidth)?newWidth:imgPtr->bmHDR->dwWidth;
+            float xr = (float)imgPtr->bmHDR->dwWidth/(float)width;
+            float yr = (float)imgPtr->bmHDR->dwHeight/(float)height;
+            for(int row =0;row<height;row++)
+                for(int col=0;col<width;col++)
+                {
+                    *GetPixel(row,col,&imgNew)=
+                            *GetPixel((int)(row*yr),(int)(col*xr),imgPtr);
+                }
+        }
+    }
+    return imgNew;
 }
 
 void InitializePixelData(IMAGE* imgPtr, PIXEL desiredPixel)
 {
+    for(int row =0;row<imgPtr->bmHDR->dwHeight;row++)
+    {
+        for(int col=0;col<imgPtr->bmHDR->dwWidth;col++)
+        {
+            *(GetPixel(row,col,imgPtr))=desiredPixel;
+        }
+    }
 
 }
 
-IMAGE Crop(IMAGE*, int, int, int, int)
+IMAGE Crop(IMAGE* imgPtr, int newWidth, int newHeight, int top, int left)
 {
-
+    IMAGE imgNew={NULL,NULL};
+    unsigned int newImageSize = 0;
+    unsigned int padding = 0;
+    unsigned int width,height;
+    PIXEL startPixel={255,255,255};
+    imgNew.bmHDR=(BITMAPHDR*) malloc(sizeof(BITMAPHDR));
+    if(imgNew.bmHDR)
+    {
+        memcpy(imgNew.bmHDR,imgPtr->bmHDR,sizeof(BITMAPHDR));
+        imgNew.bmHDR->dwWidth=newWidth;
+        imgNew.bmHDR->dwHeight=newHeight;
+        padding = newWidth % 4;
+        newImageSize = (newWidth*sizeof(PIXEL)+padding)*newHeight;
+        imgNew.bmHDR->dwImageSize=newImageSize;
+        imgNew.bmHDR->dwFileSize=newImageSize+ sizeof(BITMAPHDR);
+        imgNew.bmData=(PIXEL*) malloc(newImageSize);
+        if(imgNew.bmData)
+        {
+            InitializePixelData(&imgNew,startPixel);
+            height=(newHeight<imgPtr->bmHDR->dwHeight)?newHeight:imgPtr->bmHDR->dwHeight;
+            width=(newWidth<imgPtr->bmHDR->dwWidth)?newWidth:imgPtr->bmHDR->dwWidth;
+            for(int row =0;row<height;row++)
+                for(int col=0;col<width;col++)
+                {
+                    *GetPixel(row,col,&imgNew)=*GetPixel(row+top,col+left,imgPtr);
+                }
+        }
+    }
+    return imgNew;
 }
 
 IMAGE RotateImage(IMAGE* imgPtr, double theta)
@@ -129,12 +217,27 @@ IMAGE FancyRotateImage(IMAGE* imgPtr, double theta)
 
 }
 
-void WriteImage(IMAGE*, FILE*)
+void WriteImage(IMAGE*imgPtr, FILE*f)
 {
-
+    unsigned int padding = imgPtr->bmHDR->dwWidth % 4;
+    unsigned int imageSize = (imgPtr->bmHDR->dwWidth*sizeof(PIXEL)+padding)*imgPtr->bmHDR->dwHeight;
+    if(fwrite(imgPtr->bmHDR, sizeof(BITMAPHDR),1,f)!=1)
+    {
+        printf("Failed to write image header\n");
+    }
+    else
+    {
+        if(fwrite(imgPtr->bmData,imageSize,1,f)!=1)
+        {
+            printf("Failed to write image data.\n");
+        }
+    }
 }
 
-void FreeImage(IMAGE*)
+void FreeImage(IMAGE*imgPtr)
 {
-
+    free(imgPtr->bmHDR);
+    imgPtr->bmHDR=NULL;
+    free(imgPtr->bmData);
+    imgPtr->bmData=NULL;
 }
--- bitmap/main.c
+++ bitmap/main.c
@@ -6,9 +6,15 @@ int main() {
     IMAGE img;
     memset(&img,0,sizeof(IMAGE));
     ReadImage(&img,f);
+    fclose(f);
     if(img.bmHDR!=NULL)
     {
         PrintHeader(img.bmHDR);
     }
+//    img = Crop(&img,300,200,700,600);
+    img = ResizeCanvas(&img, 400,300);
+    f = fopen("new.bmp","w");
+    WriteImage(&img,f);
+    fclose(f);
     return 0;
 }
